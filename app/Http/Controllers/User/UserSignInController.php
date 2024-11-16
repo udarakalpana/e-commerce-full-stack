654ecd7a92529ec075758f54348127b382f7dc39
kalpana udara <kalpanaudara058@gmail.com>
2024-11-16 16:23:56 +0530

    Test user have correct email and if user have wrong email response data

--- app/Http/Controllers/User/UserSignInController.php
+++ app/Http/Controllers/User/UserSignInController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserSignInController extends Controller
 {
     public function userSignIn(Request $request)
     {
+
+        $user = User::where('email', $request['email'])->first();
+
+        if (! $user) {
+            return  response()->json([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => Response::$statusTexts[Response::HTTP_NOT_FOUND],
+            ]);
+        }
+
         return response()->json([
-            'user_first_name' => 'tes',
-            'user_last_name' => 'rewe',
-            'user_token' => 'asdfwerw'
+            'user_first_name' => $user->first_name,
+            'user_last_name' => $user->last_name,
         ]);
     }
 }
--- tests/Feature/UserAuthentication/AdminAuthenticationTest.php
+++ tests/Feature/UserAuthentication/AdminAuthenticationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\UserAuthentication;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 final class AdminAuthenticationTest extends TestCase
@@ -15,6 +16,7 @@ final class AdminAuthenticationTest extends TestCase
         // A - Arrange test data for test case
         $user = User::factory()->create();
 
+
         $userLoginDetails = [
             'email' => $user->email,
             'password' => '12345678'
@@ -23,12 +25,39 @@ final class AdminAuthenticationTest extends TestCase
         // A - Action the test case
         $response = $this->post('api/user-sign-in', $userLoginDetails);
 
+
         // A - Assertion the test outcome
         $response->assertStatus(200);
         $response->assertJsonStructure([
             'user_first_name',
             'user_last_name',
-            'user_token'
+        ]);
+        $response->assertExactJson([
+            'user_first_name' => $user->first_name,
+            'user_last_name' => $user->last_name,
+        ]);
+
+    }
+
+    public function test_if_user_not_existing_should_return_bad_response()
+    {
+        // A - Arrange
+        $userLoginDetails = [
+            'email' => 'kalpana@gmail.com',
+            'password' => '12345678'
+        ];
+
+        // A - Action the test case
+        $response = $this->post('api/user-sign-in', $userLoginDetails);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'message'
+        ]);
+        $response->assertExactJson([
+            'status' => Response::HTTP_NOT_FOUND,
+            'message' => Response::$statusTexts[Response::HTTP_NOT_FOUND],
         ]);
     }
 }
